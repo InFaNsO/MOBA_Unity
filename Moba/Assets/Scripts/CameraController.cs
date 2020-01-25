e9ccf9176749cb45dae35aff5278ba60287cab32
GuptaBhavil <421632@AIV.EDMC.STU>
2020-01-25 12:58:04 -0800

    Initial coomit

---
+++ Moba/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(h, 0.0f, v) * moveSpeed * Time.deltaTime;
+    }
+}
